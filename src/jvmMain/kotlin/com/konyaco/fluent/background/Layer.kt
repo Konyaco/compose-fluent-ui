61652d5dd77b25a58cdac1568205207ef01342fc
KonYaco <atwzj233@gmail.com>
2022-12-06 20:35:16 +0800

    Add RadioButton

--- src/jvmMain/kotlin/com/konyaco/fluent/background/Layer.kt
+++ src/jvmMain/kotlin/com/konyaco/fluent/background/Layer.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -30,7 +31,7 @@ fun Layer(
     color: Color = Colors.Background.Layer.Default,
     contentColor: Color = contentColorFor(color),
     border: BorderStroke? = null,
-    borderInside: Boolean = false,
+    outsideBorder: Boolean = false,
     elevation: Dp = 0.dp,
     content: @Composable () -> Unit
 ) {
@@ -40,8 +41,8 @@ fun Layer(
                 modifier.shadow(elevation, shape, clip = false)
                     .composed { if (border != null) Modifier.border(border, shape) else Modifier }
                     .clip(shape)
-                    .composed { if (!borderInside) Modifier.padding((1.2).dp) else Modifier } // TODO: A better way to implement outside border
-                    .background(color = color, shape = RoundedCornerShape(2.5.dp)), // TODO: A better way to set content corner
+                    .composed { if (outsideBorder) Modifier.padding((1.2).dp) else Modifier } // TODO: A better way to implement outside border
+                    .background(color = color, shape = if (shape != CircleShape) RoundedCornerShape(2.5.dp) else shape), // TODO: A better way to set content corner
                 propagateMinConstraints = true
             ) {
                 content()
---
+++ src/jvmMain/kotlin/com/konyaco/fluent/component/RadioButton.kt
@@ -0,0 +1,117 @@
+package com.konyaco.fluent.component
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsHoveredAsState
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.unit.dp
+import com.konyaco.fluent.FluentTheme
+import com.konyaco.fluent.animation.FluentDuration
+import com.konyaco.fluent.animation.FluentEasing
+import com.konyaco.fluent.background.Layer
+import com.konyaco.fluent.color.Colors
+
+@Composable
+fun RadioButton(
+    selected: Boolean,
+    onClick: (() -> Unit)?,
+    modifier: Modifier = Modifier,
+    label: String? = null,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+) {
+    // TODO: Extract same logic
+    val hovered by interactionSource.collectIsHoveredAsState()
+    val pressed by interactionSource.collectIsPressedAsState()
+
+    Row(modifier.composed {
+        if (label != null) Modifier.defaultMinSize(minWidth = 120.dp)
+        else Modifier
+    }.clickable(interactionSource, null) {
+        onClick?.invoke()
+    }) {
+        val fillColor by animateColorAsState(
+            if (selected) when {
+                !enabled -> Colors.Fill.Accent.Disabled
+                pressed -> Colors.Fill.Accent.Tertiary
+                hovered -> Colors.Fill.Accent.Secondary
+                else -> Colors.Fill.Accent.Default
+            } else when {
+                !enabled -> Colors.Fill.ControlAlt.Disabled
+                pressed -> Colors.Fill.ControlAlt.Quarternary
+                hovered -> Colors.Fill.ControlAlt.Tertiary
+                else -> Colors.Fill.ControlAlt.Secondary
+            },
+            tween(FluentDuration.QuickDuration, easing = FluentEasing.FastInvokeEasing)
+        )
+        Layer(
+            modifier = Modifier.size(20.dp),
+            shape = CircleShape,
+            color = fillColor,
+            outsideBorder = true,
+            border = BorderStroke(
+                1.dp,
+                color = if (selected) when {
+                    !enabled -> Colors.Fill.Accent.Disabled
+                    else -> Colors.Fill.Accent.Default
+                } else when {
+                    !enabled || pressed-> Colors.Stroke.ControlStrong.Disabled
+                    else -> Colors.Stroke.ControlStrong.Default
+                }
+            )
+        ) {
+            Box(contentAlignment = Alignment.Center) {
+                // Bullet, Only displays when selected, or is pressed
+
+                val size by animateDpAsState(
+                    if (selected) when {
+                        pressed -> 6.dp
+                        hovered -> 10.dp
+                        else -> 8.dp
+                    } else when {
+                        pressed -> 10.dp
+                        else -> 0.dp
+                    },
+                    tween(FluentDuration.QuickDuration, easing = FluentEasing.FastInvokeEasing)
+                )
+                Layer(
+                    modifier = Modifier.size(if (size == 0.dp) 0.dp else size + (2.4).dp), // TODO: Remove this 2.4dp if outside border is provided
+                    color = Colors.Text.OnAccent.Primary,
+                    border = BorderStroke(
+                        1.dp, Brush.verticalGradient(
+                            0f to Colors.Stroke.Control.Secondary,
+                            0.5f to Colors.Stroke.Control.Default
+                        )
+                    ),
+                    shape = CircleShape,
+                    outsideBorder = false,
+                    content = {}
+                )
+            }
+        }
+        label?.let {
+            Spacer(Modifier.width(8.dp))
+            Text(
+                modifier = Modifier.offset(y = (-1).dp),
+                text = it,
+                style = FluentTheme.typography.body.copy(
+                    color = if (enabled) Colors.Text.Text.Primary
+                    else Colors.Text.Text.Disabled
+                )
+            )
+        }
+    }
+}
