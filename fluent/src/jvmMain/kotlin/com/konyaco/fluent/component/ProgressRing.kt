e081a0d74abd2e9b80962f8fd4e530322e115db2
KonYaco <atwzj233@gmail.com>
2022-12-14 16:20:16 +0800

    Update ProgressRing.

--- fluent/src/jvmMain/kotlin/com/konyaco/fluent/component/ProgressRing.kt
+++ fluent/src/jvmMain/kotlin/com/konyaco/fluent/component/ProgressRing.kt
@@ -19,78 +19,91 @@ import androidx.compose.ui.unit.dp
 import com.konyaco.fluent.FluentTheme
 import com.konyaco.fluent.animation.FluentDuration
 
-data class ProgressRingSize(
-    val size: Dp,
-    val width: Dp
-) {
-    companion object {
-        val Large = ProgressRingSize(64.dp, 6.dp)
-        val Medium = ProgressRingSize(32.dp, 3.dp)
-        val Small = ProgressRingSize(16.dp, 1.5.dp)
-    }
+object ProgressRingSize {
+    val Large = 64.dp
+    val Medium = 32.dp
+    val Small = 16.dp
 }
 
 @Composable
 fun ProgressRing(
     progress: Float,
     modifier: Modifier = Modifier,
-    size: ProgressRingSize = ProgressRingSize.Medium,
+    size: Dp = ProgressRingSize.Medium,
+    width: Dp = size * 3 / 32,
     color: Color = FluentTheme.colors.fillAccent.default
 ) {
-    Box(modifier.size(size.size)) {
-        val density = LocalDensity.current
-        val widthPx by remember(density) { derivedStateOf { with(density) { size.width.toPx() } } }
-        Canvas(Modifier.fillMaxSize()) {
-            drawArc(
-                color = color,
-                startAngle = -90f,
-                sweepAngle = progress * 360f,
-                useCenter = false,
-                size = this.size,
-                style = Stroke(widthPx, cap = StrokeCap.Round)
-            )
-        }
-    }
+    ProgressRing(
+        modifier = modifier,
+        start = 0f,
+        length = progress * 360f,
+        width = width,
+        color = color,
+        size = size
+    )
 }
 
 @Composable
 fun ProgressRing(
     modifier: Modifier = Modifier,
-    size: ProgressRingSize = ProgressRingSize.Medium,
+    size: Dp = ProgressRingSize.Medium,
+    width: Dp = size * 3 / 32,
     color: Color = FluentTheme.colors.fillAccent.default
 ) {
-    Box(modifier.size(size.size)) {
+    val length by rememberInfiniteTransition().animateFloat(
+        initialValue = 0f,
+        targetValue = 180f,
+        infiniteRepeatable(
+            animation = tween(
+                easing = LinearEasing,
+                durationMillis = (FluentDuration.VeryLongDuration * 1.5f).toInt()
+            ), repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    val progress by rememberInfiniteTransition().animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        infiniteRepeatable(
+            animation = tween(
+                easing = LinearEasing,
+                durationMillis = FluentDuration.VeryLongDuration
+            )
+        )
+    )
+
+    val state by remember {
+        derivedStateOf {
+            (progress - length) to length
+        }
+    }
+
+    ProgressRing(
+        modifier = modifier,
+        start = state.first,
+        length = state.second,
+        width = width,
+        size = size,
+        color = color
+    )
+}
+
+@Composable
+private fun ProgressRing(
+    modifier: Modifier,
+    start: Float,
+    length: Float,
+    width: Dp,
+    size: Dp,
+    color: Color
+) {
+    Box(modifier.size(size)) {
         val density = LocalDensity.current
-        val widthPx by remember(density) { derivedStateOf { with(density) { size.width.toPx() } } }
-
-        val length by rememberInfiniteTransition().animateFloat(
-            initialValue = 0f,
-            targetValue = 180f,
-            infiniteRepeatable(
-                animation = tween(
-                    easing = LinearEasing,
-                    durationMillis = (FluentDuration.VeryLongDuration * 1.5f).toInt()
-                ), repeatMode = RepeatMode.Reverse
-            )
-        )
-
-        val progress by rememberInfiniteTransition().animateFloat(
-            initialValue = 0f,
-            targetValue = 360f,
-            infiniteRepeatable(
-                animation = tween(
-                    easing = LinearEasing,
-                    durationMillis = FluentDuration.VeryLongDuration
-                )
-            )
-        )
-
-        val start by remember { derivedStateOf { progress - length - 90f } }
-
+        val widthPx by remember(density) { derivedStateOf { with(density) { width.toPx() } } }
         Canvas(Modifier.fillMaxSize()) {
             drawArc(
                 color = color,
-                startAngle = start,
+                startAngle = start - 90f,
                 sweepAngle = length,
                 useCenter = false,
                 size = this.size,
