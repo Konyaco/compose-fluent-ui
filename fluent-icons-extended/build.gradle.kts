bfd48d02f5d94dff86cefed908935a1f1217cd5f
KonYaco <atwzj233@gmail.com>
2023-04-09 12:26:29 +0800

    Add maven publish task

--- fluent-icons-extended/build.gradle.kts
+++ fluent-icons-extended/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("maven-publish")
 }
 
-group = "com.konyaco.fluent"
+group = "com.konyaco"
 version = "0.0.1-dev3"
 
 kotlin {
@@ -39,5 +39,26 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "OSSRHSnapshot"
+            url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+        maven {
+            name = "OSSRH"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
     }
 }
--- fluent/build.gradle.kts
+++ fluent/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("maven-publish")
 }
 
-group = "com.konyaco.fluent"
+group = "com.konyaco"
 version = "0.0.1-dev3"
 
 kotlin {
@@ -49,5 +49,26 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "OSSRHSnapshot"
+            url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+        maven {
+            name = "OSSRH"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
     }
 }
