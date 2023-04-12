cad670e156b1f044775c5ffc267143b944c1be46
KonYaco <atwzj233@gmail.com>
2023-04-12 11:35:09 +0800

    Update compose version to 1.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("multiplatform") version "1.8.0" apply false
-    id("org.jetbrains.compose") version "1.3.1" apply false
+    id("org.jetbrains.compose") version "1.4.0" apply false
     id("com.android.library") version "7.4.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
 }
