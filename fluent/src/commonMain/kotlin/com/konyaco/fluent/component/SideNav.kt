fe983782ed2d3ae3f4e0cdb059454364d6672887
KonYaco <atwzj233@gmail.com>
2023-04-28 23:53:25 +0800

    Add missing imports

--- fluent/src/commonMain/kotlin/com/konyaco/fluent/component/SideNav.kt
+++ fluent/src/commonMain/kotlin/com/konyaco/fluent/component/SideNav.kt
@@ -5,6 +5,8 @@ import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.*
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
