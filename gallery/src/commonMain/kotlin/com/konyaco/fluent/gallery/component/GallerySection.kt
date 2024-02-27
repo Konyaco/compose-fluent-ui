99cf64a363c7fa9ce527491e9f96ad001c12db4f
KonYaco <atwzj233@gmail.com>
2024-02-28 00:27:06 +0800

    [Gallery] Let SourceCode be horizontal scrollable.

--- gallery/src/commonMain/kotlin/com/konyaco/fluent/gallery/component/GallerySection.kt
+++ gallery/src/commonMain/kotlin/com/konyaco/fluent/gallery/component/GallerySection.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.core.tween
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
@@ -17,6 +18,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -32,6 +34,7 @@ import com.konyaco.fluent.animation.FluentEasing
 import com.konyaco.fluent.background.Layer
 import com.konyaco.fluent.background.Mica
 import com.konyaco.fluent.component.Icon
+import com.konyaco.fluent.component.Scrollbar
 import com.konyaco.fluent.component.SubtleButton
 import com.konyaco.fluent.component.Text
 import com.konyaco.fluent.icons.Icons
@@ -68,9 +71,10 @@ fun GallerySection(
 
         val interactionSource = remember { MutableInteractionSource() }
         Card(
-            modifier = Modifier.fillMaxWidth().clickable(interactionSource = interactionSource, indication = null, onClick = {
-                sourceCodeExpanded = !sourceCodeExpanded
-            }),
+            modifier = Modifier.fillMaxWidth()
+                .clickable(interactionSource = interactionSource, indication = null, onClick = {
+                    sourceCodeExpanded = !sourceCodeExpanded
+                }),
             shape = FluentRoundedCornerShape(
                 bottomStart = if (sourceCodeExpanded) 0.dp else 8.dp,
                 bottomEnd = if (sourceCodeExpanded) 0.dp else 8.dp
@@ -114,7 +118,16 @@ fun GallerySection(
                 Column(Modifier.padding(16.dp, 12.dp)) {
                     Text("Kotlin", style = FluentTheme.typography.bodyStrong)
                     Spacer(Modifier.height(12.dp))
-                    SourceCode(sourceCode)
+                    val scrollState = rememberScrollState()
+                    Box(Modifier.fillMaxWidth().wrapContentHeight()) {
+                        SourceCode(
+                            modifier = Modifier.horizontalScroll(scrollState),
+                            code = sourceCode
+                        )
+                        Box(Modifier.fillMaxWidth().align(Alignment.BottomCenter)) {
+                            Scrollbar(modifier = Modifier.fillMaxWidth(), isVertical = false, adapter = com.konyaco.fluent.component.rememberScrollbarAdapter(scrollState))
+                        }
+                    }
                 }
             }
         }
