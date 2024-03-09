b9b0508ba1df9b28550559317f2c705b6de06a0f
sanlorng <fujia625@gmail.com>
2024-03-09 15:44:38 +0800

    [fluent] Fix the issue of incorrect nested Popup position calculation on Android.

--- fluent/src/androidMain/kotlin/com/konyaco/fluent/component/Popup.android.kt
+++ fluent/src/androidMain/kotlin/com/konyaco/fluent/component/Popup.android.kt
@@ -1,7 +1,11 @@
 package com.konyaco.fluent.component
 
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.input.key.KeyEvent
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntRect
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.window.PopupPositionProvider
 import androidx.compose.ui.window.PopupProperties
 
@@ -13,4 +17,42 @@ internal actual fun Popup(
     onPreviewKeyEvent: ((KeyEvent) -> Boolean)?,
     onKeyEvent: ((KeyEvent) -> Boolean)?,
     content: @Composable () -> Unit
-) = androidx.compose.ui.window.Popup(popupPositionProvider, onDismissRequest, properties, content)
+) {
+    val offset = LocalPopupOffset.current
+    val delegatePopupPositionProvider = remember(popupPositionProvider) {
+        DelegatePopupPositionProvider({ offset }, popupPositionProvider)
+    }
+    androidx.compose.ui.window.Popup(delegatePopupPositionProvider, onDismissRequest, properties) {
+        CompositionLocalProvider(
+            LocalPopupOffset provides delegatePopupPositionProvider.currentOffset,
+            content = content
+        )
+    }
+}
+
+// Workaround for android nested popup position calculate
+private val LocalPopupOffset = staticCompositionLocalOf { IntOffset.Zero }
+
+private class DelegatePopupPositionProvider(
+    val offset: () -> IntOffset,
+    val positionProvider: PopupPositionProvider
+): PopupPositionProvider {
+
+    var currentOffset by mutableStateOf(IntOffset.Zero)
+
+    override fun calculatePosition(
+        anchorBounds: IntRect,
+        windowSize: IntSize,
+        layoutDirection: LayoutDirection,
+        popupContentSize: IntSize
+    ): IntOffset {
+        return positionProvider.calculatePosition(
+            anchorBounds.translate(offset()),
+            windowSize,
+            layoutDirection,
+            popupContentSize
+        ).apply {
+            currentOffset = this
+        }
+    }
+}
