62ec4306224259b7f02a3a824f03906b0c27c7fd
sanlorng <fujia625@gmail.com>
2024-03-28 23:02:31 +0800

    [fluent] Add ToggleButton.

--- gallery/src/commonMain/kotlin/com/konyaco/fluent/gallery/screen/basicinput/ToggleButtonScreen.kt
+++ gallery/src/commonMain/kotlin/com/konyaco/fluent/gallery/screen/basicinput/ToggleButtonScreen.kt
@@ -1,20 +1,32 @@
 package com.konyaco.fluent.gallery.screen.basicinput
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import com.konyaco.fluent.component.Text
+import com.konyaco.fluent.component.ToggleButton
 import com.konyaco.fluent.gallery.annotation.Component
-import com.konyaco.fluent.gallery.component.GalleryHeader
+import com.konyaco.fluent.gallery.annotation.Sample
+import com.konyaco.fluent.gallery.component.GalleryPage
 
-@Component(index = 4)
+@Component(index = 4, description = "A button that can be switched between two states like a CheckBox.")
 @Composable
 fun ToggleButtonScreen() {
-    Column(Modifier.fillMaxSize()) {
-        GalleryHeader(
-            "ToggleButton",
-            "A ToggleButton looks like a Button, but works like a CheckBox. It typically has two states, checked (on) or unchecked (off), but can be indeterminate if the IsThreeState property is true. You can determine it's state by checking the IsChecked property."
+    GalleryPage(
+        title = "ToggleButton",
+        description = "A ToggleButton looks like a Button, but works like a CheckBox. It typically has two states, checked (on) or unchecked (off), but can be indeterminate if the IsThreeState property is true. You can determine it's state by checking the IsChecked property."
+    ) {
+        Section(
+            title = "A simple ToggleButton with text content.",
+            sourceCode = sourceCodeOfBasicToggleButton,
+            content = { BasicToggleButton() }
         )
-        // TODO:
     }
+}
+
+@Sample
+@Composable
+private fun BasicToggleButton() {
+    val (checked, setChecked) = remember { mutableStateOf(false) }
+    ToggleButton(checked, setChecked) { Text("ToggleButton") }
 }
