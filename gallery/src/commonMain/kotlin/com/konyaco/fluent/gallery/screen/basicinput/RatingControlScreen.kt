468a428403d90b601c3decffb1317199b2b2fac4
sanlorng <fujia625@gmail.com>
2024-04-11 08:24:59 +0800

    [fluent] Add RatingControl.

--- gallery/src/commonMain/kotlin/com/konyaco/fluent/gallery/screen/basicinput/RatingControlScreen.kt
+++ gallery/src/commonMain/kotlin/com/konyaco/fluent/gallery/screen/basicinput/RatingControlScreen.kt
@@ -1,20 +1,44 @@
 package com.konyaco.fluent.gallery.screen.basicinput
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import com.konyaco.fluent.component.RatingControl
+import com.konyaco.fluent.component.Text
 import com.konyaco.fluent.gallery.annotation.Component
-import com.konyaco.fluent.gallery.component.GalleryHeader
+import com.konyaco.fluent.gallery.annotation.Sample
+import com.konyaco.fluent.gallery.component.GalleryPage
 
-@Component(index = 11)
+@Component(index = 11, description = "Rate something 1 to 5 stars.")
 @Composable
 fun RatingControlScreen() {
-    Column(Modifier.fillMaxSize()) {
-        GalleryHeader(
-            "RatingControl",
-            "Rate something 1 to 5 stars."
+    GalleryPage(
+        title = "RatingControl",
+        description = "Rate something 1 to 5 stars."
+    ) {
+        Section(
+            title = "A simple RatingControl",
+            sourceCode = sourceCodeOfBasicRatingControlSample,
+            content = { BasicRatingControlSample() }
         )
-        // TODO:
+        Section(
+            title = "PlaceholderValue of RatingControl",
+            sourceCode = sourceCodeOfPlaceholderRatingControlSample,
+            content = { PlaceholderRatingControlSample() }
+        )
     }
+}
+
+@Sample
+@Composable
+private fun BasicRatingControlSample() {
+    val (value, setValue) = remember { mutableStateOf(0.5f) }
+    RatingControl(value = value, onValueChanged = setValue, caption = { Text("Your rating") })
+}
+
+@Sample
+@Composable
+private fun PlaceholderRatingControlSample() {
+    val (value, setValue) = remember { mutableStateOf(0.0f) }
+    RatingControl(value = value, onValueChanged = setValue, placeholderValue = 2f)
 }
