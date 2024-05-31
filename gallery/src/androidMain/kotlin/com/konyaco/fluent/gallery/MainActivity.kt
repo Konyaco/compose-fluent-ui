1782fe97fe0fcb36bf0f8d1b993bbe0840cb8221
sanlorng <fujia625@gmail.com>
2024-05-31 19:34:22 +0800

    [gallery] support navigator back history.

--- gallery/src/androidMain/kotlin/com/konyaco/fluent/gallery/MainActivity.kt
+++ gallery/src/androidMain/kotlin/com/konyaco/fluent/gallery/MainActivity.kt
@@ -2,14 +2,20 @@ package com.konyaco.fluent.gallery
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
+import com.konyaco.fluent.gallery.component.rememberComponentNavigator
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val componentNavigator = rememberComponentNavigator()
+            BackHandler(componentNavigator.latestBackEntry != null) {
+                componentNavigator.navigateUp()
+            }
             GalleryTheme {
-                App()
+                App(componentNavigator)
             }
         }
     }
