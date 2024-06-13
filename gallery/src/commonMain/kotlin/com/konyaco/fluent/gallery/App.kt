12adeabf4843ac8637f2825e5fd6e12978eec560
sanlorng <fujia625@gmail.com>
2024-06-13 19:17:12 +0800

    [gallery] update auto suggest box flyout.

--- gallery/src/commonMain/kotlin/com/konyaco/fluent/gallery/App.kt
+++ gallery/src/commonMain/kotlin/com/konyaco/fluent/gallery/App.kt
@@ -7,15 +7,12 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -29,7 +26,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
-import com.konyaco.fluent.CompactMode
 import com.konyaco.fluent.FluentTheme
 import com.konyaco.fluent.animation.FluentDuration
 import com.konyaco.fluent.animation.FluentEasing
@@ -39,14 +35,12 @@ import com.konyaco.fluent.component.Flyout
 import com.konyaco.fluent.component.Icon
 import com.konyaco.fluent.component.ListItem
 import com.konyaco.fluent.component.NavigationItemSeparator
-import com.konyaco.fluent.component.ScrollbarContainer
 import com.konyaco.fluent.component.SideNav
 import com.konyaco.fluent.component.SideNavItem
 import com.konyaco.fluent.component.Text
 import com.konyaco.fluent.component.TextBoxButton
 import com.konyaco.fluent.component.TextBoxButtonDefaults
 import com.konyaco.fluent.component.TextField
-import com.konyaco.fluent.component.rememberScrollbarAdapter
 import com.konyaco.fluent.gallery.component.ComponentItem
 import com.konyaco.fluent.gallery.component.ComponentNavigator
 import com.konyaco.fluent.gallery.component.components
@@ -128,35 +122,24 @@ fun App(
                     AutoSuggestBoxDefaults.suggestFlyout(
                         expanded = expandedSuggestion,
                         onDismissRequest = { expandedSuggestion = false },
-                        contentPadding = PaddingValues(),
-                        modifier = Modifier.suggestFlyoutSize()
-                    ) {
-
-                        CompactMode(false) {
-                            val state = rememberLazyListState()
-                            val adapter = rememberScrollbarAdapter(state)
-                            ScrollbarContainer(
-                                adapter = adapter
+                        modifier = Modifier.suggestFlyoutSize(),
+                        itemsContent = {
+                            items(
+                                items = searchResult.value,
+                                contentType = { "Item" },
+                                key = { it.hashCode().toString() }
                             ) {
-                                LazyColumn(contentPadding = PaddingValues(vertical = 3.dp), state = state) {
-                                    items(
-                                        items = searchResult.value,
-                                        contentType = { "Item" },
-                                        key = { it.hashCode().toString() }
-                                    ) {
-                                        ListItem(
-                                            onClick = {
-                                                navigator.navigate(it)
-                                                expandedSuggestion = false
-                                            },
-                                            text = { Text(it.name, maxLines = 1) },
-                                            modifier = Modifier.fillMaxWidth()
-                                        )
-                                    }
-                                }
+                                ListItem(
+                                    onClick = {
+                                        navigator.navigate(it)
+                                        expandedSuggestion = false
+                                    },
+                                    text = { Text(it.name, maxLines = 1) },
+                                    modifier = Modifier.fillMaxWidth()
+                                )
                             }
                         }
-                    }
+                    )
                 }
             },
             footer = {
