ec6b4949c5949d6e60f6fb650182a92637371309
sanlorng <fujia625@gmail.com>
2024-06-14 11:54:13 +0800

    [fluent] Fixed `FontIcon` crashed on Android.

--- fluent/src/androidMain/kotlin/com/konyaco/fluent/component/FontIcon.android.kt
+++ fluent/src/androidMain/kotlin/com/konyaco/fluent/component/FontIcon.android.kt
@@ -1,8 +1,12 @@
 package com.konyaco.fluent.component
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 
 @Composable
 internal actual fun ProvideFontIcon(content: @Composable () -> Unit) {
-    content()
+    CompositionLocalProvider(
+        LocalFontIconFontFamily provides null,
+        content = content
+    )
 }
