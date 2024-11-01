c393dc787fa8566a394f06133bab81a77b9ac29c
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2024-11-01 23:55:42 +0100

    Add wasmJs target
    (cherry picked from commit 9d9f310ae8a1fecb5d5dd09653923aecab3497e9)

--- build-plugin/src/main/java/com/konyaco/fluent/plugin/build/BuildExtension.kt
+++ build-plugin/src/main/java/com/konyaco/fluent/plugin/build/BuildExtension.kt
@@ -1,11 +1,14 @@
 package com.konyaco.fluent.plugin.build
 
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
+@OptIn(ExperimentalWasmDsl::class)
 fun KotlinMultiplatformExtension.applyTargets(publish: Boolean = true) {
     jvm("desktop")
     androidTarget {
         if (publish) publishLibraryVariants("release")
     }
     jvmToolchain(BuildConfig.Jvm.jvmToolchainVersion)
+    wasmJs { browser() }
 }
--- gallery/build.gradle.kts
+++ gallery/build.gradle.kts
@@ -13,6 +13,7 @@ plugins {
 
 kotlin {
     applyTargets(publish = false)
+    wasmJs { binaries.executable() }
     sourceSets {
         val commonMain by getting {
             dependencies {
