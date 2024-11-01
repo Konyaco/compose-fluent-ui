9c8cb29d834c3d21a0d4beb948c4e16035c07836
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2024-11-01 20:46:20 +0100

    Update Kotlin to 2.0.21 and all deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import com.konyaco.fluent.plugin.build.BuildConfig
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.compose) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.application) apply false
