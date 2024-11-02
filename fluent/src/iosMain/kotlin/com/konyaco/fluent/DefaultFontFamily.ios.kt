4dc8678e440986333294a1c1cd3e6f5d72553ce0
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2024-11-02 02:21:08 +0100

    Add iOS target and demo app
    (cherry picked from commit 045b30c0336337f6cea3fdac6df65f3d7471cb78)

---
+++ fluent/src/iosMain/kotlin/com/konyaco/fluent/DefaultFontFamily.ios.kt
@@ -0,0 +1,9 @@
+package com.konyaco.fluent
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontFamily
+
+@Composable
+actual fun defaultFontFamily(): FontFamily? {
+    return null
+}
