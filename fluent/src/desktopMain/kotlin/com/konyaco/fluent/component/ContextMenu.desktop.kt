7c2a1d194655fdef35c65ff2185e59d8d672dae6
Sanlorng <fujia625@gmail.com>
2024-12-02 10:28:54 +0800

    Fixed compilation error.

--- fluent/src/desktopMain/kotlin/com/konyaco/fluent/component/ContextMenu.desktop.kt
+++ fluent/src/desktopMain/kotlin/com/konyaco/fluent/component/ContextMenu.desktop.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.isAltPressed
 import androidx.compose.ui.input.key.isCtrlPressed
+import androidx.compose.ui.input.key.isMetaPressed
 import androidx.compose.ui.input.key.isShiftPressed
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.type
